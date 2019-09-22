dff0c37cd88ba7863d4b7d93158571b40e2a637d
Cormorant Programming <cormorant.programming@gmail.com>
2019-09-22 21:55:16 +0700

    defer

--- resources/views/_layouts/scripts.blade.php
+++ resources/views/_layouts/scripts.blade.php
@@ -1,35 +1,34 @@
+<script defer src="{{URL::asset('js/jquery.min.js')}}"></script>
+<script defer src="{{URL::asset('jquery-ui/jquery-ui.min.js')}}"></script>
+<script defer src="{{URL::asset('bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+<script defer src="{{URL::asset('js/modernizr.mq.min.js')}}"></script>
+<script defer src="{{URL::asset('js/carousel.js')}}"></script>
+<script defer src="{{URL::asset('js/datepicker.js')}}"></script>
+<script defer src="{{URL::asset('js/filter.js')}}"></script>
+<script defer src="{{URL::asset('js/resizings.js')}}"></script>
+<script defer src="{{URL::asset('js/turbolinks.js')}}"></script>
+<script defer src="{{URL::asset('js/turbolinksStart.js')}}"></script>
 
-<script src="{{URL::asset('js/jquery.min.js')}}"></script>
-<script src="{{URL::asset('jquery-ui/jquery-ui.min.js')}}"></script>
-<script src="{{URL::asset('bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{URL::asset('js/modernizr.mq.min.js')}}"></script>
-<script src="{{URL::asset('js/carousel.js')}}"></script>
-<script src="{{URL::asset('js/datepicker.js')}}"></script>
-<script src="{{URL::asset('js/filter.js')}}"></script>
-<script src="{{URL::asset('js/resizings.js')}}"></script>
-<script src="{{URL::asset('js/turbolinks.js')}}"></script>
-<script src="{{URL::asset('js/turbolinksStart.js')}}"></script>
+<script defer src="{{URL::asset('js/stimulus.umd.js')}}"></script>
+<script defer src="{{URL::asset('js/controllers/hello_controller.js')}}"></script>
 
-<script src="{{URL::asset('js/stimulus.umd.js')}}"></script>
-<script src="{{URL::asset('js/controllers/hello_controller.js')}}"></script>
-
-<script src="{{URL::asset('js/entities/_layout.js')}}"></script>
-<script src="{{URL::asset('js/entities/booking.js')}}"></script>
-<script src="{{URL::asset('js/entities/bookingPurpose.js')}}"></script>
-<script src="{{URL::asset('js/entities/bookingType.js')}}"></script>
-<script src="{{URL::asset('js/entities/company.js')}}"></script>
-<script src="{{URL::asset('js/entities/country.js')}}"></script>
-<script src="{{URL::asset('js/entities/customerMessage.js')}}"></script>
-<script src="{{URL::asset('js/entities/dish.js')}}"></script>
-<script src="{{URL::asset('js/entities/dishType.js')}}"></script>
-<script src="{{URL::asset('js/entities/equipment.js')}}"></script>
-<script src="{{URL::asset('js/entities/identificationType.js')}}"></script>
-<script src="{{URL::asset('js/entities/onlinePlateform.js')}}"></script>
-<script src="{{URL::asset('js/entities/room.js')}}"></script>
-<script src="{{URL::asset('js/entities/roomSize.js')}}"></script>
-<script src="{{URL::asset('js/entities/title.js')}}"></script>
-<script src="{{URL::asset('js/entities/user.js')}}"></script>
-<script src="{{URL::asset('js/entities/userType.js')}}"></script>
-<script src="{{URL::asset('js/entities/vipCard.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/_layout.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/booking.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/bookingPurpose.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/bookingType.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/company.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/country.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/customerMessage.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/dish.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/dishType.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/equipment.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/identificationType.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/onlinePlateform.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/room.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/roomSize.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/title.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/user.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/userType.js')}}"></script>
+<script defer src="{{URL::asset('js/entities/vipCard.js')}}"></script>
 
 
